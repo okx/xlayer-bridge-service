9495e0625787f132df619bfd611910ff7c36a8a1
zhangkai <zhangkai.gis@163.com>
2023-11-20 15:45:38 +0800

    use h.Bytes()

--- claimtxman/types/monitoredtx.go
+++ claimtxman/types/monitoredtx.go
@@ -115,8 +115,7 @@ func (mTx MonitoredTx) RemoveHistory(tx *types.Transaction) {
 func (mTx *MonitoredTx) HistoryHashSlice() [][]byte {
 	history := make([][]byte, 0, len(mTx.History))
 	for h := range mTx.History {
-		h := h
-		history = append(history, h[:])
+		history = append(history, h.Bytes())
 	}
 	return history
 }
