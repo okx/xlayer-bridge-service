5a1fea959f7be669cbec86d00970597e0ff9c858
zhangkai <zhangkai.gis@163.com>
2023-11-20 15:32:48 +0800

    add local var

--- claimtxman/types/monitoredtx.go
+++ claimtxman/types/monitoredtx.go
@@ -115,6 +115,7 @@ func (mTx MonitoredTx) RemoveHistory(tx *types.Transaction) {
 func (mTx *MonitoredTx) HistoryHashSlice() [][]byte {
 	history := make([][]byte, 0, len(mTx.History))
 	for h := range mTx.History {
+		h := h
 		history = append(history, h[:])
 	}
 	return history
