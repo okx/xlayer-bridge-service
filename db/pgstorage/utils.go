7fa294eb13c50dffaceb17b3958b9a91f23b266c
chengzhinei <chengxue5yuan@163.com>
2024-03-20 16:37:12 +0800

    fix e2e

--- db/pgstorage/utils.go
+++ db/pgstorage/utils.go
@@ -74,7 +74,7 @@ func InitOrReset(cfg Config) error {
 	}
 
 	// run migrations
-	if err := ResetDB(cfg); err != nil {
+	if err := RunMigrationsDown(cfg); err != nil {
 		return err
 	}
 	return RunMigrationsUp(cfg)
