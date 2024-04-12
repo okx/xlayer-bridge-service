26640f6fc04e731721e54541f2da68c4bf5e0996
trunghai95 <bthai.1995@gmail.com>
2024-04-12 15:18:06 +0800

    Change log lib

--- xxljobs/executor.go
+++ xxljobs/executor.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"time"
 
-	"github.com/0xPolygonHermez/zkevm-node/log"
+	"github.com/0xPolygonHermez/zkevm-bridge-service/log"
 	"github.com/xxl-job/xxl-job-executor-go"
 )
 
