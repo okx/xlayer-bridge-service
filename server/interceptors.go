9c0997e11b2f168e4acc21d67e76567bd19e1227
trunghai95 <bthai.1995@gmail.com>
2024-04-19 15:52:12 +0800

    Test check IP interceptor

--- server/interceptors.go
+++ server/interceptors.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/0xPolygonHermez/zkevm-bridge-service/log"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
@@ -23,5 +24,16 @@ func NewRequestLogInterceptor() grpc.UnaryServerInterceptor {
 		respJson, _ := protojson.Marshal(resp.(proto.Message))
 		log.Infof("method[%v] req[%v] resp[%v] err[%v] processTime[%v]", methodName, string(reqJson), string(respJson), err, duration.String())
 		return resp, err
+	}
+}
+
+func NewIPCheckInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		methodName := info.FullMethod
+		if headers, ok := metadata.FromIncomingContext(ctx); ok {
+			xForwardedFor := headers.Get("x-forwarded-for")
+			log.Debugf("method[%v] xForwardedFor[%v]", methodName, xForwardedFor)
+		}
+		return handler(ctx, req)
 	}
 }
